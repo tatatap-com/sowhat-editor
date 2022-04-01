bd92ec5b2728bf41535280d0ae03a68c8baf0b77
Greg Skiano <gregoryskiano@gmail.com>
2022-04-01 11:07:10 -0700

    rearranging

--- main.js
+++ main.js
@@ -1,42 +1,87 @@
-import createTapEditor from "./src/createTapEditor";
+import './reset.css';
+import './main.css';
+import sowhat from '@tatatap-com/sowhat';
+import createTapEditor from "./lib/index";
 
-const wrap = document.getElementById('app');
+////////////////////////
+// CONFIGURE EXAMPLES //
+////////////////////////
 
-for (let i = 0; i < 2; i++) {
-  const n = document.createElement('div');
-  n.style.margin = '20px';
-  wrap.appendChild(n);
+const examples = [
+  '*123 2022-04-01 /foo/bar this $$ummm)'
+];
+
+///////////////
+// UTILITIES //
+///////////////
+
+const notesRoot = document.getElementById('notes');
+const SPACE_PATTERN = /\s/;
+const makeOption = v => ({ name: v, value: SPACE_PATTERN.test(v) ? `"${v}"` : v });
+const makeOptions = names => names.map(makeOption);
+const makeFolders = (folderMap) => {
+  return Object.entries(folderMap).map(([k, v]) => {
+    const f = makeOption(k);
+    if (v !== true) f.children = makeFolders(v);
+    return f;
+  });
+};
+
+function create(tag, attr = {}, children = []) {
+  children = Array.isArray(children) ? children : [children];
+  const elm = document.createElement(tag);
+  for (let a in attr) {
+    if (a.startsWith('on')) {
+      elm.addEventListener(a.slice(2).toLowerCase(), attr[a])
+    } else {
+      if (attr[a]) elm.setAttribute(a, attr[a]);
+    }
+  }
+  children.forEach((c) => {
+    if (typeof c === 'string') {
+      elm.innerText = c;
+    } else {
+      elm.appendChild(c);
+    }
+  });
+  return elm;
+};
+
+//////////////////////
+// RUN THE EXAMPLES //
+//////////////////////
+
+const exampleRoot = create('div', { class: 'ex-examples' });
+
+const addNote = (initialValue) => {
+  const editor = create('div', { class: 'ex-editor' })
+  const output = create('p', { class: 'ex-output' });
 
   createTapEditor({
-    root: n,
-    initialValue: '*123 2022-04-01 /foo/bar this $$ummm)',
-    beans: [
-      { name: 'income', value: 'income' },
-      { name: 'donation', value: 'donation' }
-    ],
-    events: [
-      { name: 'eating out', value: '"eating out"' },
-      { name: 'pushups', value: 'pushups' }
-    ],
-    tagnames: [
-      { name: 'inspiration', value: 'inspiration' },
-      { name: 'wanna buy', value: '"wanna buy"' }
-    ],
-    folders: [
-      { name: 'work', value: 'work',
-        children: [
-          { name: 'todos', value: 'todos' },
-          { name: 'memos', value: 'memos' }
-        ]
-      },
-      { name: 'Baz Qux', value: '"Baz Qux"',
-        children: [
-          { name: 'more here', value: '"more here"' }
-        ]
-      }
-    ]
-  });
+    root: editor,
+    initialValue,
+    onChange: (v) => { output.innerText = JSON.stringify(sowhat.parse(v), null, 2) },
+  })
+
+  exampleRoot.appendChild(create('div', { class: 'ex-wrap'}, [
+    editor,
+    create('details', { class: 'ex-details' }, [
+      create('summary', {}, 'Parser output'),
+      output,
+    ])
+  ]));
 }
 
+notesRoot.appendChild(create('div', {}, [
+  create('h2', { class: 'panel-title' }, 'Notes' ),
+  create('div', { class: 'example-panel-content' }, [
+    exampleRoot,
+    create('button', {
+      class: 'ex-add-note',
+      onclick: () => addNote(),
+    }, 'Add note'),
+  ])
+]));
 
 
+examples.forEach(addNote);
