4057b5620b335a007d9c26fb6ecae6064f6243c1
joe k <call@1800joe.com>
2022-08-02 15:29:36 -0400

    adding base to vite config to make gh base path work in deployed context

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ command, mode }) => {
   if (command === 'build' && mode === 'production') {
     return {
+      base: '/sowhat-editor/',
       build: {
         emptyOutDir: false,
         lib: {
